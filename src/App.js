dea36f5c441bdfce2f00cdca60e7d28d8bac2096
sent1nel101 <37023270+sent1nel101@users.noreply.github.com>
2021-12-31 16:25:58 -0500

    another react todo list practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React, { useState} from 'react'
 
-function App() {
+export default function App() {
+  const [newTodo, setNewTodo] = useState('')
+  const [todos, setTodos] = useState([])
+
+function handleNewTodoChange (e){
+  e.preventDefault()
+  setNewTodo(e.target.value)
+}
+function handleNewTodo(e){
+  e.preventDefault()
+  console.log(todos)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ToDoList">
+      <h1>ToDo</h1>
+      <form onSubmit={handleNewTodo}>
+        <input placeholder="Your todo list..." onchange={handleNewTodoChange}/>
+        <ul>
+          <li>Buy bread</li>
+          <li>Get dry cleaning</li>
+          <li>Clean car</li>
+          <li>Meet Danny at park</li>
+        </ul>
+      </form>
     </div>
   );
 }
-
-export default App;
