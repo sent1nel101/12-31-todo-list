e3d890ebfed7d93e0c857a80967fde80205daddc
sent1nel101 <37023270+sent1nel101@users.noreply.github.com>
2021-12-31 23:12:57 -0500

    style updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState} from 'react'
 export default function App() {
   const [newTodo, setNewTodo] = useState('')
   const [todos, setTodos] = useState([])
+  var errorMessage = ""
 
 function handleNewTodoChange (e){
   e.preventDefault()
@@ -11,24 +12,40 @@ function handleNewTodoChange (e){
 }
 function handleNewTodo(e){
   e.preventDefault()
-  if (newTodo === "") return
+  if (newTodo === "") {
+    alert("Please enter some text.")
+    return
+  }else{
   setTodos([...todos, {id: Date.now(), text: newTodo}])
   e.target.reset()
+  setNewTodo('')
+}
 }
 function removeTodo(id){
-  setTodos(todos.filter((todo)=> todo.id != id))
+  var answer = window.confirm("Are you sure?")
+  if (answer){
+    setTodos(todos.filter((todo)=> todo.id !== id))
+}else {
+  return
+}
+}
+function saveBtnClick(){
+  handleNewTodo()
 }
   return (
     <div className="toDoList">
-      <h1>ToDo</h1>
+      <h1>ToDo Tracker</h1>
       <form onSubmit={handleNewTodo}>
-        <input placeholder="Your todo list..." onChange={handleNewTodoChange}/>
+        <div className="inputDiv">
+          <input placeholder="Your todo list..." onChange={handleNewTodoChange}/>
+          <button id="saveBtn" type="submit"><img id="plusIcon" src="./plus.png" /></button>
+        </div>
         <ul>
         {todos.map((todo) => (
           <li key=
           {todo.id}>
           {todo.text}
-          <a href="#" onClick={() => removeTodo(todo.id)}>X</a>
+          <a id="clearBtn" href="#" onClick={() => removeTodo(todo.id)}><img id="cancelImg" src="https://www.clipartmax.com/png/middle/307-3077324_x-mark-cross-computer-icons-clip-art-red-cross-icon.png" /></a>
           </li>
         ))}
         </ul>
